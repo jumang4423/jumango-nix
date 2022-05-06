387a09604ad436d0d7019d461d3c85a7e3004de1
jumang4423 <catontheskate4423@gmail.com>
2022-05-06 13:07:03 +0900

    st but luke build

--- scripts/pull_git_and_update_by_current_stamp.sh
+++ scripts/pull_git_and_update_by_current_stamp.sh
@@ -6,3 +6,6 @@ git pull origin master
 sudo cp -f ./nixp/configuration.nix /etc/nixos/configuration.nix
 cp -f ./nixp/.config/nixpkgs/home.nix ~/.config/nixpkgs/home.nix
 cp -f ./nixp/.config/i3/config ~/.config/i3/config
+
+sudo nixos-rebuild switch --install-bootloader
+home-manager switch
