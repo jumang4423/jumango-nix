f1c8fe1efc6870db2210cdd061863808f9b881d3
jumang4423 <catontheskate4423@gmail.com>
2022-05-06 15:01:21 +0900

    added rust

--- scripts/pull_git_and_update_by_current_stamp.sh
+++ scripts/pull_git_and_update_by_current_stamp.sh
@@ -6,9 +6,9 @@ git pull origin master
 sudo cp -f ./nixp/configuration.nix /etc/nixos/configuration.nix
 rm -rf ~/.config/nixpkgs
 mkdir -p ~/.config/nixpkgs/pkgs
-cp -f ./nixp/.config/nixpkgs ~/.config
+cp -rf ./nixp/.config/nixpkgs ~/.config
 mkdir -p ~/.config/i3
-cp -f ./nixp/.config/i3/config ~/.config/i3/config
+cp -rf ./nixp/.config/i3/config ~/.config/i3/config
 
 sudo nixos-rebuild switch --install-bootloader
 home-manager switch
